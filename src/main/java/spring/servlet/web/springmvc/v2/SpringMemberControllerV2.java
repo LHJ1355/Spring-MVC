9d12a6f1f898bc0e254427ea6a8a9534277c9db7
LHJ1355 <lhj56411355@gmail.com>
2021-07-19 22:28:30 +0900

    spring mvc controller

--- src/main/java/spring/servlet/web/springmvc/v2/SpringMemberControllerV2.java
+++ src/main/java/spring/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -1,7 +1,11 @@
-package spring.servlet.web.springmvc.v1;
+package spring.servlet.web.springmvc.v2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import spring.domain.Member;
 import spring.domain.MemberRepository;
@@ -10,10 +14,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
-public class SpringMemberSaveControllerV1 {
+@RequestMapping("/springmvc/v2/members")
+public class SpringMemberControllerV2 {
+
     private final MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping("/springmvc/v1/members/save")
+    @RequestMapping("/new-form")
+    public ModelAndView process() {
+        return new ModelAndView("new-form");
+    }
+
+    @RequestMapping("/save")
     public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
         String username = request.getParameter("username");
         int age = Integer.parseInt(request.getParameter("age"));
